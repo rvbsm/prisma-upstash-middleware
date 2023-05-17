cd82e18d7c051c98cdc849c47f8c844d18a8904e
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-05-17 16:01:11 +0300

    refactor: edit cast dating function

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,10 @@ type JSONType = string | number | object | [] | boolean | null;
 
 const datePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z$/;
 
-function castDates<T extends JSONType>(_: string, value: T): T | Date {
-	const isDate = typeof value === "string" && datePattern.test(value);
-	return isDate ? new Date(value) : value;
+function dateReviver(_: string, value: JSONType): JSONType | Date {
+	return typeof value === "string" && datePattern.test(value)
+		? new Date(value)
+		: value;
 }
 
 function upstashMiddleware(options: MiddlewareOptions): Prisma.Middleware {
@@ -38,8 +39,8 @@ function upstashMiddleware(options: MiddlewareOptions): Prisma.Middleware {
 
 		const cache = await upstash.get<string | object>(key);
 		if (cache !== null) {
-			if (typeof cache === "string") return JSON.parse(cache, castDates);
-			else return JSON.parse(JSON.stringify(cache), castDates);
+				if (typeof cache === "string") return JSON.parse(cache, dateReviver);
+				else return JSON.parse(JSON.stringify(cache), dateReviver);
 		}
 
 		const result = await next(params);
